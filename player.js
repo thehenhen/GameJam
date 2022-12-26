2fdc04755c1b2aba7f3e96c0d78ba6615ccb1496
Henry Zhang <henry.zhang6@student.tdsb.on.ca>
2022-12-26 18:33:40 -0500

    staircases

--- player.js
+++ player.js
@@ -11,15 +11,25 @@ class player{
 
     update(){
         if(this.left){
-            this.x-=10;
+            this.x-=5;
             console.log("A");
         } else if(this.right){
-            this.x+=10;
+            this.x+=5;
             console.log("D");
         }
         if(this.up){
             this.y-=20;
             console.log("W");
+        }
+        if((this.y>280 || this.y+this.vY>280) && this.x>=80 && this.x<=110){
+            this.y=280;
+            this.up=false;
+            this.vY=0;
+        }
+        if((this.y>260 || this.y+this.vY>260) && this.x>=100 && this.x<=320){
+            this.y=260;
+            this.up=false;
+            this.vY=0;
         }
         if(this.y>300 || this.y+this.vY>300){
             this.y=300;
@@ -74,7 +84,7 @@ function keyReleased(){
 }
 
 let p;
-/*
+
 function setup(){
     createCanvas(1800,500);
     p = new player();
@@ -84,7 +94,9 @@ function draw(){
     background(0);
     fill(100);
     rect(0,350,1800,150);
+    rect(100,330,20,20);
+    rect(120,310,200,40);
     p.update();
     p.show();
     console.log(p.vY);
-}*/
+}
