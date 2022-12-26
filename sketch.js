5d317d47ab4920b6723d2559562707e359eb1eef
3xp3rtz <a343063004@gmail.com>
2022-12-26 16:56:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 function setup(){
     createCanvas(400,400);
+    playerLoc = [100,200];
 }
 function draw(){
     background(0);
     fill(255);
-    ellipse(100,100,100,100); 
+    ellipse(100,100,100,100);
+    circle(playerLoc[0],playerLoc[1],10);
 }
